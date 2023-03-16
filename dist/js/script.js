08113dc8f749bdda2a3b49e214db3dcc8aec9b0b
Alexandr <manlain1231@gmail.com>
2023-03-16 15:20:11 +0300

    forth section

--- dist/js/script.js
+++ dist/js/script.js
@@ -9,3 +9,10 @@ hamburger.addEventListener('click', () => {
 close.addEventListener('click', () => {
     menu.classList.remove('active');
 });
+
+const percents = document.querySelectorAll('.skills__scale-percent'),
+    lines = document.querySelectorAll('.skills__scale-row span');
+
+percents.forEach( (item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
